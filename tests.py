ecd96011bcc6ba7937d83e10b6b49304ceef03e3
Richard van Dijk <37830964+MarinusVanDijk@users.noreply.github.com>
2021-03-16 15:28:57 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -33,7 +33,7 @@ class TestsHelperFunctions(unittest.TestCase):
     def test_closest_command(self):
         invalid_command = ""
         closest = hedy.closest_command(invalid_command, ['ask', 'print', 'echo'])
-        self.assertEqual(closest, 'ask')
+        self.assertEqual(closest, 'assk')
 
         invalid_command = "print"
         closest = hedy.closest_command(invalid_command, ['print', 'ask', 'echo'])
